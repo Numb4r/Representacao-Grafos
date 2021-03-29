36970e43eaf2198dd4754318161a0df604aed0d0
Numb4r <yuridimitre@protonmail.com>
2021-03-29 20:40:27 -0300

    atualizado main

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,12 @@
 #include "AdjMatrix.hpp"
-#include <cstdio>
+#include "IncMatrix.hpp"
+
 int main(int argc, char const *argv[])
 {
-	
-    
-    AdjMatrix::Graph pGraph(10);
-    pGraph.insertEdge(0,2);
-	pGraph.insertEdge(0,1);
-	pGraph.insertEdge(1,0);
-	pGraph.insertEdge(1,4);
-	pGraph.insertEdge(1,5);
-	pGraph.insertEdge(2,0);
-	pGraph.insertEdge(2,3);
-	pGraph.insertEdge(3,2);
-	pGraph.insertEdge(4,1);
-	pGraph.insertEdge(4,5);
-	pGraph.insertEdge(4,6);
-	pGraph.insertEdge(5,1);
-	pGraph.insertEdge(5,4);
-	pGraph.insertEdge(5,6);
-	pGraph.insertEdge(5,7);
-	pGraph.insertEdge(6,4);
-	pGraph.insertEdge(6,5);
-	pGraph.insertEdge(6,7);
-	pGraph.insertEdge(7,6);
-	pGraph.insertEdge(7,5);
+    // IncMatrix::Graph pGraph("graph.txt");
+    AdjMatrix::Graph pGraph("graph.txt");	
     pGraph.foo();
+    pGraph.bar();
     pGraph.BFS();
 	pGraph.DFS();
     return 0;
