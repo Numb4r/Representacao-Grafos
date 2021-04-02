edf142174933ae98753d3f4981dec259f8dde5ba
Numb4r <yuridimitre@protonmail.com>
2021-04-02 11:24:08 -0300

    Script agora da sort no formato correto

--- GraphRandom.sh
+++ GraphRandom.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 #usage: random.sh NUMBERVERTEX NUMBEREDGES
 RANDOM=$$
-text=""
 graph="digraph random{\n"
-for ((i=0;i<$2;i=i+1))
-do
-    s1=$((RANDOM%$1))
+printf "" > graph.txt
+for ((i=0;i<$2;i=i+1));do
+    s1=$((RANDOM%$1)) 
     s2=$((RANDOM%$1))
-    text="${text}${s1},${s2}\n"
+    printf "${s1} ${s2}\n" >>graph.txt
     graph="$graph${s1} -> ${s2};\n"
 done
+
+
+
 graph="${graph}}"
-printf $text > graph.txt;
 printf "${graph}" > graph.dot
 dot -Tpng graph.dot -o graph.png
 
-sort -h graph.txt -o graph.txt
-sed -i "1 i $1,$2" graph.txt
+sort -k 1n -k 2n graph.txt -o graph.txt
+sed -i "1 i $1 $2" graph.txt
