12c9059105390f7cc7b83bd351d626e94f7776d7
Numb4r <yuridimitre@protonmail.com>
2021-04-02 17:20:53 -0300

    Corrigido entradas duplicadas

--- GraphRandom.sh
+++ GraphRandom.sh
@@ -6,6 +6,14 @@ printf "" > graph.txt
 for ((i=0;i<$2;i=i+1));do
     s1=$((RANDOM%$1)) 
     s2=$((RANDOM%$1))
+    while : ; do 
+        if grep -xq "^${s1} ${s2}$" graph.txt; then
+            s1=$((RANDOM%$1)) 
+            s2=$((RANDOM%$1))
+        else 
+            break 
+        fi
+    done
     printf "${s1} ${s2}\n" >>graph.txt
     graph="$graph${s1} -> ${s2};\n"
 done
@@ -14,7 +22,7 @@ done
 
 graph="${graph}}"
 printf "${graph}" > graph.dot
-dot -Tpng graph.dot -o graph.png
+# dot -Tpng graph.dot -o graph.png
 
 sort -k 1n -k 2n graph.txt -o graph.txt
 sed -i "1 i $1 $2" graph.txt
